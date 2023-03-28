a2b6a1c156f1a0cfb28abb89e9d9e15cb04bc41f
shahih96 <118007390+shahih96@users.noreply.github.com>
2023-03-28 10:14:57 +0700

    Update tugas no 2.py

--- tugas 4/tugas no 2.py
+++ tugas 4/tugas no 2.py
@@ -1,175 +1,104 @@
-import random
-
 class Robot:
+
     jumlah_turn = 0
 
     def __init__(self, nama, health, damage):
         self.nama = nama
         self.health = health
         self.damage = damage
+        
+    def lakukan_aksi(self,nama):
+        if nama == "Antares": 
+            if self.jumlah_turn % 3 == 0:
+                damage = self.damage * 1.5  
+            else:
+                damage = self.damage
 
-    def lakukan_aksi(self):
-        if self.jumlah_turn % self.aksi_interval == 0 and self.jumlah_turn != 0:
-            self.tambah_darah()
-            self.damage_temp = self.damage * self.damage_multiplier
+        elif nama == "Alphasetia":
+            if self.jumlah_turn % 2 == 0:
+                self.health += 4000  
+                print(f'Robot lawan ({nama}) menambah darah sebanyak 4000 HP')
+            damage = self.damage
+
+        elif nama == "Lecalicus":
+            if self.jumlah_turn % 4 == 0:
+                self.health += 7000
+                print(f'Robot lawan ({nama}) menambah darah sebanyak 7000 HP')
+                damage = self.damage * 2        
+            else:
+                damage = self.damage
+
         else:
-            self.damage_temp = self.damage
-
-        print(f"{self.nama} menyerang sebanyak {self.damage_temp} DMG")
-        return self.damage_temp
-
-    def terima_aksi(self, damage):
+            damage = self.damage
+        return damage
+    
+    def terima_aksi(self,damage, status,nama):
         self.health -= damage
-        if self.health <= 0:
-            print(f"{self.nama} kalah")
-            return False
-        else:
-            return True
+        if status == 1:
+            print(f'Robotmu ({nama}) menyerang sebanyak {int(damage)} DMG')
+        elif status == 2:
+            print(f'Robot lawan ({nama}) menyerang sebanyak {int(damage)} DMG')
 
 class Antares(Robot):
-    base_health = 50000
-    base_attack = 5000
-    aksi_interval = 3
-    damage_multiplier = 1.5
-    damage_temp = base_attack
-
-    def tambah_darah(self):
-        self.health += 0
-
+    def __init__(self, nama, health, damage):
+        super().__init__(nama, health, damage)
+        
 class Alphasetia(Robot):
-    base_health = 40000
-    base_attack = 6000
-    aksi_interval = 2
-    damage_multiplier = 1
-    damage_temp = base_attack
-
-    def tambah_darah(self):
-        self.health += 4000
+    def __init__(self, nama, health, damage):
+        super().__init__(nama, health, damage)
 
 class Lecalicus(Robot):
-    base_health = 45000
-    base_attack = 5500
-    aksi_interval = 4
-    damage_multiplier = 2
-    damage_temp = base_attack
-
-    def tambah_darah(self):
-        self.health += 7000
-
-
-def main():
-    print("Selamat datang di pertandingan robot Yamako")
-    pilihan_robotmu = int(input("Pilih robotmu (1 = Antares, 2 = Alphasetia, 3 = Lecalicus): "))
-    pilihan_lawan = int(input("Pilih robot lawan (1 = Antares, 2 = Alphasetia, 3 = Lecalicus): "))
-    robotmu = None
-    lawan = None
-
-    if pilihan_robotmu == 1:
-        robotmu = Antares("Antares", Antares.base_health, Antares.base_attack)
-    elif pilihan_robotmu == 2:
-        robotmu = Alphasetia("Alphasetia", Alphasetia.base_health, Alphasetia.base_attack)
-    elif pilihan_robotmu == 3:
-        robotmu = Lecalicus("Lecalicus", Lecalicus.base_health, Lecalicus.base_attack)
-
-    if pilihan_lawan == 1:
-        lawan = Antares("Antares", Antares.base_health, Antares.base_attack)
-    elif pilihan_lawan == 2:
-        lawan = Alphasetia("Alphasetia", Alphasetia.base_health, Alphasetia.base_attack)
-    elif pilihan_lawan == 3:
-        lawan = Lecalicus("Lecalicus", Lecalicus.base_health, Lecalicus.base_attack)
-
-    tangan_robotmu = int(input("Pilih tangan robotmu (" + robotmu.nama + "): "))
-    tangan_lawan = random.randint(1, 3)
-    print(f"Pilih tangan robot lawan ({lawan.nama}): {tangan_lawan}")
-
-    while robotmu.health > 0 and lawan.health > 0:
-        if tangan_robotmu == tangan_lawan:
-            print("Seri!")
-        elif tangan_robotmu == 1 and tangan_lawan == 2:
-            lawan.health -= robotmu.damage
-            print(f"{robotmu.nama} menang dan menyerang sebanyak {robotmu.damage} DMG")
-        if lawan.health > 0:
-            lawan.lakukan_aksi(robotmu)
-        elif tangan_robotmu == 2 and tangan_lawan == 3:
-            lawan.health -= robotmu.damage
-            print(f"{robotmu.nama} menang dan menyerang sebanyak {robotmu.damage} DMG")
-        if lawan.health > 0:
-            lawan.lakukan_aksi(robotmu)
-        elif tangan_robotmu == 3 and tangan_lawan == 1:
-            lawan.health -= robotmu.damage
-            print(f"{robotmu.nama} menang dan menyerang sebanyak {robotmu.damage} DMG")
-        if lawan.health > 0:
-            lawan.lakukan_aksi(robotmu)
+    def __init__(self, nama, health, damage):
+        super().__init__(nama, health, damage)
+        
+def pertarungan(p1,p2):
+    while True:
+        if p1.health <= 0:
+            print("robotmu kalah setelah",p1.jumlah_turn,"turn")
+            break
+        if p2.health <= 0:
+            print("robotmu menang setelah",p1.jumlah_turn,"turn")
+            break
+            
+        p1.jumlah_turn +=1
+        p2.jumlah_turn +=1
+        print("Turn saat ini : ", p1.jumlah_turn)
+        print("Robotmu (",p1.nama,"-",p1.health,"HP),robot lawan (",p2.nama,"-",p2.health,"HP)")
+        
+        a = int(input(f'Pilih tangan robotmu ({p1.nama}): '))
+        b = int(input(f'Pilih tangan robot lawan ({p2.nama}): '))
+        if (a < 4 and a > 0) and (b < 4 and b > 0):
+            if (a == 1 and b == 3) or (a == 2 and b == 1) or (a == 3 and b == 2):
+                aksi1 = p1.lakukan_aksi(p1.nama)
+                p2.terima_aksi(aksi1, 1,p1.nama)
+            elif (a == 3 and b == 1) or (a == 1 and b == 2) or (a == 2 and b == 3):
+                aksi2 = p2.lakukan_aksi(p2.nama)
+                p1.terima_aksi(aksi2, 2, p2.nama)
+            elif (a == 1 and b == 1) or (a == 2 and b == 2) or (a == 3 and b == 3):
+                print("Draw Try Again!!!!!!") 
         else:
-            robotmu.health -= lawan.damage
-            print(f"{lawan.nama} menang dan menyerang sebanyak {lawan.damage} DMG")
-        if robotmu.health > 0:
-            robotmu.lakukan_aksi(lawan)
-
-
-    # increment jumlah_turn setiap turn
-    robotmu.jumlah_turn += 1
-    lawan.jumlah_turn += 1
-
-    # check apakah sudah saatnya Alphasetia dan Lecalicus melakukan aksi khusus
-    if isinstance(robotmu, Alphasetia) and robotmu.jumlah_turn % 2 == 0:
-        robotmu.tambah_darah(4000)
-        print(f"{robotmu.nama} menambah darah sebanyak 4000 HP")
-
-    if isinstance(robotmu, Lecalicus) and robotmu.jumlah_turn % 4 == 0:
-        robotmu.tambah_darah(7000)
-        robotmu.damage *= 2
-        print(f"{robotmu.nama} menambah darah sebanyak 7000 HP dan menaikkan damage menjadi {robotmu.damage} DMG")
-
-    if isinstance(lawan, Alphasetia) and lawan.jumlah_turn % 2 == 0:
-        lawan.tambah_darah(4000)
-        print(f"{lawan.nama} menambah darah sebanyak 4000 HP")
-
-    if isinstance(lawan, Lecalicus) and lawan.jumlah_turn % 4 == 0:
-        lawan.tambah_darah(7000)
-        lawan.damage *= 2
-        print(f"{lawan.nama} menambah darah sebanyak 7000 HP dan menaikkan damage menjadi {lawan.damage} DMG")
-
-    # check apakah permainan telah berakhir
-    if robotmu.health <= 0:
-        print(f"{robotmu.nama} kalah dan keluar dari pertandingan!")
-        print(f"Selamat kepada {lawan.nama} yang memenangkan pertandingan!")
+            print("tangan robotmu tidak ada")
+        
+print("Selamat datang di pertandingan robot Yamako")
+while True:
+    robotmu = int(input("Pilih robotmu (1=Antares, 2=Alphasetia, 3=Lecalicus):"))
+    robot_lawan = int(input("Pilih robot lawan (1=Antares, 2=Alphasetia, 3=Lecalicus):"))
+    if (robotmu < 4 and robotmu > 0) and (robot_lawan < 4 and robot_lawan > 0):
+        if robotmu == 1:
+            p1 = Antares("Antares",50000,5000)
+        elif robotmu == 2:
+            p1 = Alphasetia("Alphasetia",40000,6000)
+        elif robotmu == 3:
+            p1 = Lecalicus("Lecalicus",45000,5500)
+        if robot_lawan == 1:
+            p2 = Antares("Antares",50000,5000)
+        elif robot_lawan == 2:
+            p2 = Alphasetia("Alphasetia",40000,6000)
+        elif robot_lawan == 3:
+            p2 = Lecalicus("Lecalicus",45000,5500)
         break
-
-    if lawan.health <= 0:
-        print(f"{lawan.nama} kalah dan keluar dari pertandingan!")
-        print(f"Selamat kepada {robotmu.nama} yang memenangkan pertandingan!")
-        break
-
-# input tangan_robotmu dan tangan_lawan untuk turn selanjutnya
-tangan_robotmu = int(input(f"\nTurn saat ini: {robotmu.jumlah_turn}\nRobotmu ({robotmu.nama} - {robotmu.health} HP), robot lawan ({lawan.nama} - {lawan.health} HP)\nPilih tangan robotmu ({robotmu.nama}): "))
-print(f"Pilih tangan robot lawan ({lawan.nama}): {tangan_lawan}")
-
-
-# Lakukan aksi berdasarkan hasil dari permainan
-if tangan_robotmu == tangan_lawan:
-    print("Hasil imbang!")
-elif (tangan_robotmu == 1 and tangan_lawan == 3) or (tangan_robotmu == 2 and tangan_lawan == 1) or (tangan_robotmu == 3 and tangan_lawan == 2):
-    print(f"Robotmu ({robotmu.nama}) menang!")
-    lawan.health -= robotmu.damage
-    robotmu.jumlah_turn += 1
-    robotmu.lakukan_aksi(lawan)
-else:
-    print(f"Robot lawan ({lawan.nama}) menang!")
-    robotmu.health -= lawan.damage
-    lawan.jumlah_turn += 1
-    lawan.lakukan_aksi(robotmu)
-
-# Periksa kondisi kesehatan robot setelah melakukan aksi
-if robotmu.health <= 0:
-    print(f"\n{robotmu.nama} kalah!")
-    break
-elif lawan.health <= 0:
-    print(f"\n{lawan.nama} kalah!")
-    break
-
-#output
-if robotmu.health > 0:
-    print(f"\nSelamat, {robotmu.nama} menang!")
-else:
-    print(f"\nSayang sekali, {robotmu.nama} kalah!")
+    else:
+        print("robot tidak tersedia")
+        
+print("Pilih 1 untuk batu, 2 untuk kertas, dan 3 untuk gunting")
+pertarungan(p1,p2)
